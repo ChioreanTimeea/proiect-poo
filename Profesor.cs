b79196fec7cb981713d0061cf2dcade2460df64e
ChioreanTimeea <timeea.chiorean@yahoo.com>
2025-01-06 12:07:23 +0200

    Update Profesor.cs

--- Profesor.cs
+++ Profesor.cs
@@ -1,13 +1,17 @@
+
+        
 using System.Globalization;
+using System.Reflection.Metadata;
 
 namespace proiect_poo;
 
-public class Profesor:Utilizator
+public class Profesor : Utilizator
 
 {
-    public Profesor(string email ,string nrMatricol,string numePers,string parola):
-        base(email,nrMatricol,numePers,parola)
-    {}
+    public Profesor(string email, string nrMatricol, string numePers, string parola) :
+        base(email, nrMatricol, numePers, parola)
+    {
+    }
 
     public void DeschidereSesiune(List<sesiune> sesiuni)
     {
@@ -28,16 +32,88 @@ public class Profesor:Utilizator
         {
             sesiune.EsteDeschisa = false;
             Console.WriteLine("Sesiunea a fost inchisa");
-            
+
         }
         else
         {
-           Console.WriteLine("Sesiunea nu a fost gasita");
+            Console.WriteLine("Sesiunea nu a fost gasita");
         }
-        
+    }
+
+    public void VizualizareListaProiecteStudenti(List<proiect> proiecte)
+
+    {
+        if (proiecte.Count == 0)
+        {
+            Console.WriteLine("Lista cu proiecte este goala");
+            return;
+        }
+
+        Console.WriteLine("Lista proiectelor studentilor: ");
+        foreach (var proiect in proiecte)
+        {
+            Console.WriteLine(
+                $"Student : {proiect.Nume},Titlu: {proiect.Titlu},Descriere:{proiect.Descriere},a fost notat cu nota {proiect.Nota}");
+        }
+    }
+
+    public void NotareProiect(List<proiect> proiecte, string NumeStudent, string TitluProiect, float Nota)
+    {
+        var proiect = proiecte.Find(p => p.Nume == NumeStudent  &&  p.Titlu == TitluProiect);
+        if (proiect != null)
+        {
+            proiect.Nota = Nota;
+            Console.WriteLine(
+                $"Proiectul: {proiect.Titlu} al studentului {proiect.Nume}este notat cu nota {proiect.Nota}");
+        }
+        else
+        {
+            Console.WriteLine("Proiectul nu a fost gasit");
+            return;
+
+        }
+    }
+
+    public void RaspunsReclamatie(List<reclamatie> reclamatii, string reclamatieId, string raspuns)
+    {
+        var reclamatie = reclamatii.Find(r => r.Id == reclamatieId);
+        if (reclamatie == null)
+        {
+            Console.WriteLine("Reclamatia nu a fost gasita");
+            return;
+        }
+        else
+        {
+            reclamatie.Raspuns = raspuns;
+            reclamatie.Rezolvat = true;
+            Console.WriteLine($"Raspunsul la reclamatie a fost inregistrat");
+            
+
+        }
+    }
+
+    public void ModificareNota(List<proiect>proiecte,string NumeStudent,string TitluProiect,int NotaNoua)
+    {
+        var proiect = proiecte.Find(p => p.Nume == NumeStudent && p.Titlu == TitluProiect);
+        if (proiect == null)
+        {
+            Console.WriteLine("Proiectul nu a fost gasit");
+            return;
+        }
+        else
+        {
+            proiect.Nota = NotaNoua;
+            Console.WriteLine ($"Proiectul: {proiect.Titlu} al studentului {proiect.Nume} ,nota a fost modificata cu nota {proiect.Nota}");
+        }
 
     }
+    
 }
+    
+    
+
+
+
     
 
 
